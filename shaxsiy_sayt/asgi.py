46e7431523e5887ee58d104f2de337f25c37c72d
asliddin3101 <kamoliddinovasliddin90@gmail.com>
2021-07-31 15:16:30 +0300

    Bu shaxsiy sayt

---
+++ shaxsiy_sayt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for shaxsiy_sayt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shaxsiy_sayt.settings')
+
+application = get_asgi_application()
---
+++ shaxsiy_sayt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shaxsiy_sayt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shaxsiy_sayt.settings')
+
+application = get_wsgi_application()
